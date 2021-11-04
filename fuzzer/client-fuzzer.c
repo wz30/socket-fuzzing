de83f573ec70d727a7c7b1b0e021b2183b69837f
Wei zhang <wz146@duke.edu>
2021-11-04 14:46:32 -0400

    init with simple server and client codes

---
+++ fuzzer/client-fuzzer.c
@@ -0,0 +1,86 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#define MAX 80
+#define PORT 8087
+#define SA struct sockaddr
+// function for chat
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		bzero(buff, sizeof(buff));
+		printf("Enter the string : ");
+		n = 0;
+		while ((buff[n++] = getchar()) != '\n')
+			;
+		write(sockfd, buff, sizeof(buff));
+		bzero(buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));
+		printf("From Server : %s", buff);
+		if ((strncmp(buff, "exit", 4)) == 0) {
+			printf("Client Exit...\n");
+			break;
+		}
+	}
+}
+
+int client_fuzzer(const char *Data, long long size)
+{
+	int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+	// socket create and varification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("152.3.69.238");
+	servaddr.sin_port = htons(PORT);
+
+	// connect the client socket to server socket
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+		printf("connection with the server failed...\n");
+		exit(0);
+	}
+	else
+		printf("connected to the server..\n");
+
+	// function for chat
+	func(sockfd);
+
+	// close the socket
+	close(sockfd);
+}
+
+
+
+// int socket_test(const char *Data, long long size)
+// {
+// 	int socket_desc;
+// 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	
+// 	if (socket_desc == -1)
+// 	{
+// 		printf("Could not create socket");
+// 	}
+// 	else {
+// 		printf("--create socket");	
+// 	}
+	
+// 	return 0;
+// }
+int LLVMFuzzerTestOneInput(const char *Data, long long Size) {
+  client_fuzzer(Data, Size);
+  return 0;
+}

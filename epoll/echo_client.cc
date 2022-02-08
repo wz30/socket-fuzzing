3f63df3a00575b00130e55d58a2d882b6818064f
wei zhang <wz146@duke.edu>
2022-02-08 04:15:55 +0000

    init version for epoll

--- epoll/echo_client.cc
+++ epoll/echo_client.cc
@@ -1,12 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
 
 #define BUF_SIZE 1024
 void error_handling(char *message);
+
+std::vector<std::string> split(std::string str, char del) {
+  std::vector<std::string> internal; 
+  std::stringstream ss(str); // Turn the string into a stream. 
+  std::string tok; 
+ 
+  while(getline(ss, tok, del)) { 
+    internal.push_back(tok); 
+  } 
+ 
+  return internal;
+}
+
+int fake_set(int id, int obj_size) {
+  std::cout << "inside fake set function" <<std::endl;
+  if(id < 0) return -1;
+  if(obj_size <=0) return -1;
+  // sleep(1);
+  return 1;
+}
+
+int fake_get(int id) {
+  std::cout << "inside fake get function" <<std::endl;
+  if(id < 0) return -1;
+  // sleep(1);
+  return 1;
+}
+
+int fake_delete(int id) {
+  std::cout << "inside fake delete function" <<std::endl;
+  if(id < 0) return -1;
+  // sleep(1);
+  return 1;
+}
+
+
+
+// return status -1, -2, normal number
+/*
+  -3: incorrect usage of set get commond
+  -2: commonad not container [user]
+  -1: wrong status from lightning
+
+*/
+int process_msg(char *message){
+  int status =  -1;
+  if(std::string(message).find("set") != std::string::npos) {
+    std::vector<std::string> sep = split(message, ' ');
+    if(sep.size() != 3) {
+      return -3;
+    }
+    // for(std::string t : sep) {
+    //   std::cout << t << std::endl;
+    // }
+    // std::cout << std::stoi(sep[2]) << std::endl;
+    status = fake_set(std::stoi(sep[1]), std::stoi(sep[2]));
+  } else if(std::string(message).find("get") != std::string::npos) {
+
+    std::vector<std::string> sep = split(message, ' ');
+    if(sep.size() != 2) {
+      return -3;
+    }
+    status = fake_get(std::stoi(sep[1]));
+
+  } else if(std::string(message).find("delete") != std::string::npos) {
+
+    std::vector<std::string> sep = split(message, ' ');
+    if(sep.size() != 2) {
+      return -3;
+    }
+    status = fake_delete(std::stoi(sep[1]));
+
+  } else if(std::string(message).find("mput") != std::string::npos) {
+
+  } else if(std::string(message).find("mget") != std::string::npos) {
+
+  } else if(std::string(message).find("mupdate") != std::string::npos) {
+
+  } else {
+    std::cout << "unrecognized commond from master" <<std::endl;
+    return -2;
+  }
+
+  return status;
+}
 
 int main(int argc, char *argv[])
 {
@@ -37,16 +126,40 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        fputs("Input message(Q to quit): ", stdout);
-        fgets(message, BUF_SIZE, stdin);
+//        fputs("Input message(Q to quit): ", stdout);
+//        fgets(message, BUF_SIZE, stdin);
 
         if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
             break;
 
-        write(sock, message, strlen(message));
-        str_len = read(sock, message, BUF_SIZE - 1);
+//        write(sock, message, strlen(message));
+        str_len = recv(sock, message, BUF_SIZE - 1, 0);
+	//std::cout << "recving from server " << str_len << std::endl; 
         message[str_len] = 0;
         printf("Message from server: %s", message);
+	// todo check if contains user message
+	if( std::string(message).find("[user]") != std::string::npos)
+	{
+	    std::cout << "processing the message and interacting with lightning." << std::endl;
+	    std::cout << message << std::endl;
+	     
+	    // calling  lightning api to process the message
+	    int status = -1;
+	    char state[4];
+	    status = process_msg(message);
+	    if (status < 0) {
+		//return error status
+		sprintf(state, "%d", status);
+		send(sock, state, std::strlen(state), 0);
+	    } else {
+		sprintf(state, "%d", status);
+		send(sock, state, std::strlen(state), 0);
+
+	    }
+	    std::cout << "send is finished" << std::endl;
+	}else {
+	    std::cout << "incorrect message from server" << std::endl; 
+	}
     }
     close(sock);
     return 0;
